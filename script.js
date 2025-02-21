fa80f004c695b8e9f6ffbce282fd3b3d9b8e92b4
jeroboam-francis <francisjeroboam@gmail.com>
2025-02-22 02:34:24 +0300

    Rock,paper,scissors game

---
+++ script.js
@@ -0,0 +1,50 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerEmoji = document.querySelector(".player");
+const computerEmoji = document.querySelector(".computer");
+
+
+
+let playerScore = 0;
+let computerScore = 0;
+const playerScoreDisplay = document.getElementById("playerScore");
+const computerScoreDisplay = document.getElementById("computerScore");
+
+const emojis = {
+    rock: "✊",
+    paper: "✋",
+    scissors: "✌️"
+};
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = "";
+    if (playerChoice === computerChoice) {
+        result = "draw"
+    }
+    else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "PLAYER WINS";
+        playerScore++;
+    }
+
+    else {
+        result = "COMPUTER WINS";
+        computerScore++;
+    }
+
+
+    playerEmoji.textContent = emojis[playerChoice];
+    computerEmoji.textContent = emojis[computerChoice];
+
+    resultDisplay.textContent = result;
+
+    playerScoreDisplay.textContent = ` ${playerScore} : Player`;
+    computerScoreDisplay.textContent = `Computer : ${computerScore}`;
+
+}
